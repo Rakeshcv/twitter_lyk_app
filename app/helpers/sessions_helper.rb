a059321461dc1d30253e1e42efdd2e25b6367cfd
Rakesh C V <rakesh.cv@hotmail.com>
2015-10-24 09:11:40 +1100

    Finish log in/log out

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,45 @@
+module SessionsHelper
+
+#Logs in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #Return the current logged-in user, if any.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.token_authenticate?(cookie.permanent[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Returns true if user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Logout mechanism
+  def log_out
+    forget current_user
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # Forgets a User
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def rememeber(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   has_secure_password
@@ -9,4 +11,33 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX, allow_blank: true}, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}, presence: true
+
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forgets a User
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Return true if the give token matches the digest
+  def token_authenticate?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
